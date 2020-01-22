627531fd0a534fc9e6ee6111697b723e8bc2c7f5
Cem Aydin <cem.aydin@gmx.ch>
2020-01-22 19:27:39 +0100

    add media dir as volume, docker command, readme

--- update.sh
+++ update.sh
@@ -2,11 +2,13 @@
 #
 
 cp db/db.json db.json_backup
+git pull
 docker build -t reflect .
 docker stop reflect
 docker rm reflect
 docker run -d -p 4040:4040 \
   --mount type=bind,source="$(pwd)"/db,target=/usr/src/app/db \
+  --mount type=bind,source="$(pwd)"/media,target=/usr/src/app/client/dist/media \
   --restart always \
   --name reflect \
   reflect
