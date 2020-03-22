3d6e991634fe91fee59062907b15411e858ce6d3
Cem Aydin <cem.aydin@gmx.ch>
2020-03-22 23:46:19 +0100

    rename docker image/cont. to reflectapp

--- update.sh
+++ update.sh
@@ -3,12 +3,12 @@
 
 cp db/db.json db.json_backup
 git pull
-docker build -t reflect .
-docker stop reflect
-docker rm reflect
+docker build -t reflectapp .
+docker stop reflectapp
+docker rm reflectapp
 docker run -d -p 4040:4040 \
   --mount type=bind,source="$(pwd)"/db,target=/usr/src/app/db \
   --mount type=bind,source="$(pwd)"/media,target=/usr/src/app/client/dist/media \
-  --restart always \
-  --name reflect \
-  reflect
+  --restart unless-stopped \
+  --name reflectapp \
+  reflectapp
